8577d4fc52ab3635b705b9aee2cd71840f7fdd1b
gabriel nzilantuzu <gabrielnzilantuzu@gmail.com>
2025-01-16 18:13:00 +0300

    changed the image repo

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.IntegerField()
+    title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
 
