dae5fa1cd82d5aa04c5031778e2d62d6664debf0
gabriel nzilantuzu <gabrielnzilantuzu@gmail.com>
2025-01-16 17:43:18 +0300

    changed the modal

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.IntegerField()
     description = models.TextField()
     completed = models.BooleanField(default=False)
 
